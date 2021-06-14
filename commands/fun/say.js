8394211c1b1c18d6b56db6f3cbaf5247506efb5b
Flo-Gr <florian.garnier@epitech.eu>
2021-06-14 14:32:11 +0200

    Adding functional help command

--- commands/fun/say.js
+++ commands/fun/say.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: "say",
-    description: "say",
+    description: "I say therefore I live",
     execute(client, message, args, command) {
         const say = args.join(" ");
         if (message.author.id != "650432748275892253" && message.author.id != "175948123183644672") {
@@ -11,8 +11,8 @@ module.exports = {
         } else if (message.guild === null) {
             return message.reply("I can't do that in private messages...");
         } else {
-            message.delete({ setTimeout: 1 });
             message.channel.send(`${say}`);
+            message.delete();
         }
     }
 }
--- commands/utility/help.js
+++ commands/utility/help.js
@@ -1,8 +1,44 @@
+const { promises: fs } = require("fs");
+
+async function getFiles(path) {
+    const entries = await fs.readdir(path, { withFileTypes: true });
+
+    const files = entries
+        .filter(file => !file.isDirectory())
+        .map(file => ({ ...file, path: path + file.name}));
+    const folders = entries.filter(folder => folder.isDirectory());
+    let final_array = Array(0);
+
+    for (const folder of folders) {
+        const tmp_array = await getFiles(`${path}${folder.name}/`);
+        for (let index = 0; index < tmp_array.length; index++)
+            final_array.push(tmp_array[index]);
+    }
+    let path_array = Array(0);
+    for (let index = 0; index < files.length; index++) {
+        const element = files[index];
+        path_array.push(element.path);
+    }
+    return path_array.length != 0 ? path_array : final_array;
+}
+
+async function help_command(message)
+{
+    let commands_array = await getFiles("./commands/");
+    let final_message = "Here is a list of my commands :\n\n";
+
+    for (let index = 0; index < commands_array.length; index++) {
+        const element = require(`../../${commands_array[index]}`);
+        final_message += ("Name : " + element.name + "\nDescription : " + element.description + "\n\n");
+    }
+    message.channel.send(`${final_message}`);
+}
+
+
 module.exports = {
     name: "help",
-    description: "help",
+    description: "Help me, HELP ME, N**GA",
     execute(client, message, args, command) {
-        const commands = message.client;
-        message.client.send(`${commands.map(command => command.name).join(`: ${command.description}\n`)}`);
+        help_command(message);
     }
 }
